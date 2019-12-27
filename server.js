7e4d013a039d2be9b03831c5da2df80acd3cad7a
colenocks <enockscoleman@gmail.com>
2019-12-27 22:18:09 +0000

    resolved issues

--- server.js
+++ server.js
@@ -11,16 +11,7 @@ const session = require("express-session");
 const hostname = process.env.HOST;
 const port = process.env.PORT;
 
-<<<<<<< HEAD
-const IN_PROD = NODE_ENV === "production";
-
-app.use(express.static("dist"));
-app.use(express.static("assets"));
-// Virtual Path Prefix '/static'
-// app.use('/images', express.static('public'))
-=======
 app.use(express.static(path.join(__dirname, "dist")));
->>>>>>> replace const variables with environment variables
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
