85d0ad6cde86c53a80e77907d319e3d589b8f7e9
Cole Enocks <enockscoleman@gmail.com>
2020-11-20 23:12:18 +0000

    style: redesign MainNav with materialize, add rrd link for optimization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 import MainNav from "./components/Navigation/MainNav/MainNav";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className='main__container'>
       <MainNav />
       <main className='main__content'>
-        <Home />
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
       </main>
       <Footer />
     </div>
--- frontend/src/components/Navigation/MainNav/MainNav.js
+++ frontend/src/components/Navigation/MainNav/MainNav.js
@@ -1,43 +1,76 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./MainNav.css";
 
 const MainNav = () => {
   const image_url =
     "https://res.cloudinary.com/icardi/image/upload/v1577780709/cyobLogo_200x200_gy6opt.png";
   return (
-    <nav className='nav__header'>
-      <div className='logo__div'>
-        <a href='/'>
-          <img src={image_url} alt='Logo' height='80px' width='80px' />
-        </a>
-        <p>
-          <a href='/'>Cycle of Benefits</a>
-        </p>
-        {/* Fit MobileMenu component here */}
-        <div className='nav__menu'>
-          <ul className='nav__list'>
-            <li className='nav__item'>
-              <a href='/'>Home</a>
-            </li>
-            <li className='nav__item'>
-              <a href='/projects'>Projects</a>
-            </li>
-            <li className='nav__item'>
-              <a href='/about'>About</a>
-            </li>
-          </ul>
+    <React.Fragment>
+      <nav className='teal darken-3 z-depth-2'>
+        <div className='container'>
+          <div className='nav-wrapper'>
+            <Link to='/' className='brand-logo'>
+              <img
+                id='logo'
+                src={image_url}
+                alt='Logo'
+                height='80px'
+                width='100px'
+              />
+            </Link>
+            <Link to='#!' data-target='mobile-menu' className='sidenav-trigger'>
+              <i className='material-icons'>menu</i>
+            </Link>
+            <ul id='nav-mobile' className='right hide-on-med-and-down'>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/projects'>Projects</Link>
+              </li>
+              <li>
+                <Link to='/about'>About</Link>
+              </li>
+              <li>
+                <Link to='/login'>Login</Link>
+              </li>
+              <li>
+                <Link to='/register'>Sign Up</Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className='signin'>
-        <a className='signin__link' href='/login'>
-          Login
-        </a>
-        <a className='signin__link' href='/register'>
-          Sign Up
-        </a>
-      </div>
-    </nav>
+      </nav>
+
+      <ul className='sidenav' id='mobile-menu'>
+        <li>
+          <Link className='sidenav-close' to='/'>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className='sidenav-close' to='/projects'>
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link className='sidenav-close' to='/about'>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link className='sidenav-close' to='/login'>
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link className='sidenav-close' to='/register'>
+            Sign Up
+          </Link>
+        </li>
+      </ul>
+    </React.Fragment>
   );
 };
-
 export default MainNav;
