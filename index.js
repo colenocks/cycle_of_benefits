a8b61090305f0c74d4afb707ce017e2294667ee8
Cole Enocks <enockscoleman@gmail.com>
2020-12-22 18:57:18 +0000

    add build script, modify index.js

--- index.js
+++ index.js
@@ -6,55 +6,20 @@ const cors = require("cors");
 
 const path = require("path");
 const bodyParser = require("body-parser");
-const session = require("express-session");
 
 const adminRoutes = require("./routes/admin");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const projectRoutes = require("./routes/project");
 
-const hostname = process.env.HOST;
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 const { mongoConnect } = require("./database/connection");
 
-app.use(express.static(path.join(__dirname, "dist")));
-app.use(express.json({ limit: "1mb" }));
-app.use(express.urlencoded({ extended: true }));
+// Parse Data
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//view engine set up
-app.set("views", path.join(__dirname, "views"));
-app.engine("html", require("ejs").__express);
-app.set("view engine", "html");
-app.set("view engine", "ejs");
-
-// Session Custom options
-app.use(
-  session({
-    secret: process.env.SESS_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: parseInt(process.env.SESS_LIFETIME, 10),
-      sameSite: true,
-      // secure: process.env.IN_PROD
-    },
-  })
-);
-
-//CORS SETUP
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
-
-// OR
 app.use(cors());
 
 //Register routes
@@ -63,27 +28,16 @@ app.use(authRoutes);
 app.use(userRoutes);
 app.use(projectRoutes);
 
-// Errors : Page not found
-app.use((req, res, next) => {
-  var err = new Error("Page not found");
-  err.status = 404;
-  next(err);
-});
-
-//Handle errors
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.send(err.message);
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+}
 
 //connect to server only when connection to the database is established
 mongoConnect((err, client) => {
   if (err) {
     console.log("Connection to DB unsuccessful");
   }
-  app.listen(port, () =>
-    console.log(`Listening at ${hostname} on port:${port}`)
-  );
+  app.listen(port, () => console.log(`Listening on port:${port}`));
 });
 
 module.exports = app;
